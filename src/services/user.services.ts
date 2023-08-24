57890c7fb6d43f0c0263f68f1343f95c4e1776e4
hoangvu6262 <vucao@namlongsoft.net>
2023-08-24 15:45:16 +0700

    authentication model, service and controller

---
+++ src/services/user.services.ts
@@ -0,0 +1,28 @@
+import User from '../models/user.model'
+
+const getUserByID = (id: string) => {
+    return User.findById(id)
+}
+
+const getUserByName = (username: string) => {
+    return User.findOne({ username })
+}
+
+const getUserByEmail = (email: string) => {
+    return User.findOne({ email })
+}
+
+const creatNewUser = (user: Record<string, any>) => {
+    return User.create(user)
+}
+
+const getAllUsers = (id: string) => {
+    return User.find({ _id: { $ne: id } }).select([
+        'email',
+        'username',
+        'avatarImage',
+        '_id',
+    ])
+}
+
+export { getUserByID, getUserByName, getUserByEmail, creatNewUser, getAllUsers }
