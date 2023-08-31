f9102c4e0f2a98141fc27af82c84c1a9885ac8da
hoangvu6262 <vucao@namlongsoft.net>
2023-09-01 00:37:31 +0700

    add channel model, service and controller

--- src/routes/root.route.ts
+++ src/routes/root.route.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import authRoute from './auth.route'
 import serverRoute from './server.route'
+import channelRoute from './channel.route'
 
 const router = express.Router()
 
 export default (): express.Router => {
     authRoute(router)
     serverRoute(router)
+    channelRoute(router)
     return router
 }
