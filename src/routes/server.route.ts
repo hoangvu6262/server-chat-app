bfdde9da47b9a8e4d4c21ae6a0f04d5cb7f37fa5
hoangvu6262 <vucao@namlongsoft.net>
2023-09-06 14:34:45 +0700

    update server controller

--- src/routes/server.route.ts
+++ src/routes/server.route.ts
@@ -3,12 +3,14 @@ import {
     getAllServerByUserId,
     createNewServerByUser,
     getServerById,
+    updateServerById,
 } from '../controllers/server.controller'
 
 const serverRoute = (router: Router) => {
     router.get('/server/by-userId/:userId', getAllServerByUserId)
     router.get('/server/by-serverId/:serverId', getServerById)
     router.post('/server', createNewServerByUser)
+    router.put('/server/:id', updateServerById)
 }
 
 export default serverRoute
